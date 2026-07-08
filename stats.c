ee0611986d4f08752ae9dfdf4eedfbeab6171669
M-ALMERI <mohammedalmerri101@gmail.com>
2026-07-08 23:52:45 +0300

    Add running statistics tracker

--- stats.c
+++ stats.c
@@ -1,1 +1,62 @@
 #include "stats.h"
+
+#include <math.h>
+
+void stats_tracker_reset(StatsTracker *tracker) {
+    tracker->count = 0;
+    tracker->sum = 0.0;
+    tracker->sum_squares = 0.0;
+    tracker->min = 0.0;
+    tracker->max = 0.0;
+}
+
+void stats_tracker_add(StatsTracker *tracker, double value) {
+    if (tracker->count == 0) {
+        tracker->min = value;
+        tracker->max = value;
+    } else {
+        if (value < tracker->min) {
+            tracker->min = value;
+        }
+
+        if (value > tracker->max) {
+            tracker->max = value;
+        }
+    }
+
+    tracker->count++;
+    tracker->sum += value;
+    tracker->sum_squares += value * value;
+}
+
+double stats_tracker_mean(const StatsTracker *tracker) {
+    if (tracker->count == 0) {
+        return 0.0;
+    }
+
+    return tracker->sum / tracker->count;
+}
+
+double stats_tracker_rms(const StatsTracker *tracker) {
+    if (tracker->count == 0) {
+        return 0.0;
+    }
+
+    return sqrt(tracker->sum_squares / tracker->count);
+}
+
+double stats_tracker_stddev(const StatsTracker *tracker) {
+    if (tracker->count == 0) {
+        return 0.0;
+    }
+
+    double mean = stats_tracker_mean(tracker);
+    double mean_square = tracker->sum_squares / tracker->count;
+    double variance = mean_square - (mean * mean);
+
+    if (variance < 0.0) {
+        variance = 0.0;
+    }
+
+    return sqrt(variance);
+}
--- stats.h
+++ stats.h
@@ -1,4 +1,20 @@
 #ifndef STATS_H
 #define STATS_H
 
+#include <stddef.h>
+
+typedef struct {
+    size_t count;
+    double sum;
+    double sum_squares;
+    double min;
+    double max;
+} StatsTracker;
+
+void stats_tracker_reset(StatsTracker *tracker);
+void stats_tracker_add(StatsTracker *tracker, double value);
+double stats_tracker_mean(const StatsTracker *tracker);
+double stats_tracker_rms(const StatsTracker *tracker);
+double stats_tracker_stddev(const StatsTracker *tracker);
+
 #endif
